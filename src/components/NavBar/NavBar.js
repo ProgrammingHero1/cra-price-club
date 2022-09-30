0fa4d673c765ead1df9382dabd35dfb84acf2d70
Programming Hero <programming.hero1@gmail.com>
2022-09-29 23:16:53 -0500

    toggle nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,20 +5,20 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 const NavBar = () => {
     const [open, setOpen] = useState(false);
     const routes = [
-        {id: 1, name: 'Home', path: '/home'},
-        {id: 2, name: 'Products', path: '/home'},
-        {id: 3, name: 'Orders', path: '/home'},
-        {id: 4, name: 'Contact', path: '/home'},
-        {id: 5, name: 'About', path: '/home'},
+        { id: 1, name: 'Home', path: '/home' },
+        { id: 2, name: 'Products', path: '/products' },
+        { id: 3, name: 'Orders', path: '/orders' },
+        { id: 4, name: 'Contact', path: '/contact' },
+        { id: 5, name: 'About', path: '/about' },
     ]
     return (
-        <nav>
-            {
-                open ? 
-                <XMarkIcon onClick={() => setOpen(!open)} className="h-6 w-6"/> 
-                : <Bars3Icon onClick={() => setOpen(!open)} className="h-6 w-6"/>
-            }
-            <ul className='md:flex justify-center'>
+        <nav className='bg-purple-200 w-full'>
+            <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden">
+                {
+                    open ? <XMarkIcon /> : <Bars3Icon />
+                }
+            </div>
+            <ul className={`bg-purple-200 md:flex w-full justify-center absolute md:static duration-500 ease-in ${open ? 'top-6' : 'top-[-120px]'}`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
